9e2ad0ff5babbedb144f36644853f585ff8cc4c6
taylorstewart <Taylor@Taylors-MBP.att.net>
2015-11-26 13:38:53 -0500

    ggplot2 visualizations

--- calorimetry_analysis.R
+++ calorimetry_analysis.R
@@ -9,7 +9,7 @@
 ## ===========================================================
 ## Source Data_Init Script ... get ems_cal data.frame
 ## ===========================================================
-source("Data_Init.R")
+source("data_init.R")
 ems_cal
 
 fit1 <- lm(wet_HOC_JG~wet_wt*basin,data=ems_cal)
@@ -30,9 +30,21 @@ fitPlot(fit3,legend="bottomright")
 fit4 <- lm(wet_HOC_JG~basin*month,data=ems_cal)
 coef(fit4)
 anova(fit4)
-fitPlot(fit4,which="basin",ylim=c(8000,11000),xlab="Basin",ylab="J/G Wet Basis")
-fitPlot(fit4,which="month",ylim=c(8000,11000),xlab="Month",ylab="J/G Wet Basis")
-fitPlot(fit4,ylim=c(7000,12000),xlab="Basin",ylab="J/G Wet Basis")
-fitPlot(fit4,change.order=TRUE,ylim=c(7000,12000),legend="topleft",xlab="Month",ylab="J/G Wet Basis")
+fitPlot(fit4,which="basin",ylim=c(8000,11000),xlab="",ylab="Energy Density (J/g Wet Basis)")
+fitPlot(fit4,which="month",ylim=c(8000,11000),xlab="",ylab="Energy Density (J/g Wet Basis)")
+fitPlot(fit4,ylim=c(7000,12000),xlab="",ylab="Energy Density (J/g Wet Basis)")
+fitPlot(fit4,change.order=TRUE,ylim=c(7000,12000),legend="topleft",xlab="",ylab="Energy Density (J/g Wet Basis)")
 
-boxplot(wet_HOC_JG~basin*month,data=ems_cal,ylim=c(5000,14650),ylab="J/G Wet Basis",boxwex=0.5)
+ggplot(ems_cal,aes(basin,wet_HOC_JG)) +
+  geom_boxplot() +
+  xlab("") +
+  ylab("Energy Density (J/g Wet Basis)") +
+  theme_bw() +
+  facet_wrap(~month)
+
+ggplot(ems_cal,aes(month,wet_HOC_JG)) +
+  geom_boxplot() +
+  xlab("") +
+  ylab("Energy Density (J/g Wet Basis") +
+  theme_bw() +
+  facet_wrap(~basin)

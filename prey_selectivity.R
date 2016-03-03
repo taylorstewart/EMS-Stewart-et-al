a6d6c099cb6554489ae8faa10c7271ada2287e46
taylorstewart <trstewart@usgs.gov>
2016-03-03 11:14:56 -0500

    added comparative section

--- prey_selectivity.R
+++ prey_selectivity.R
@@ -31,6 +31,7 @@ ems_prey <- bind_rows(ems_zoop,ems_benthos) %>%
 ## -----------------------------------------------------------
 serial_list <- as.character(unique(ems_diet$serial))
 region_list <- as.character(unique(ems_prey$region))
+basin_list <- as.character(unique(ems_prey$basin))
 month_list <-  c('May','September')
 fid_list <- unique(ems_diet$fid)
 diet_list <- unique(ems_diet$food_item)
@@ -136,14 +137,86 @@ ems_prey_select %<>% mutate("diet/envir" = diet_prop/envir_prop)
 ## -----------------------------------------------------------
 ## Calculate alpha ((r/p)/sum(r/p))
 ## -----------------------------------------------------------
-final_select <- as.data.frame(do.call(rbind,lapply(fid_list,function(k) {
+alpha <- data.frame(do.call(rbind,lapply(fid_list,function(k) {
   indiv <- ems_prey_select %>% filter(fid == k)
   serial <- unique(as.character(indiv$serial))
-  region <- unique(as.character(indiv$region))
+  month <- unique(as.character(indiv$month))
+  basin <- unique(as.character(indiv$basin))
   taxa <- as.character(indiv$food_item)
   n <- nrow(indiv)
     tmp <- data.frame(do.call(rbind,lapply(1:n,function(p) {
       round(indiv[p,12]/sum(indiv[,12]),5)
     })))
-    data.frame(fid=rep(k,n),serial=serial,region=region,taxa=taxa,alpha=tmp)
+  data.frame(fid=rep(k,n),serial,month,basin,taxa,alpha=tmp$diet.envir)
 })))
+## Remove NAs and summarize mean alpha by month, basin, and taxa
+alpha %<>% filter(!is.na(alpha)) %>% 
+  group_by(month,basin,taxa) %>% 
+  summarize(alpha=mean(alpha))
+
+## -----------------------------------------------------------
+## Clean up environment
+## -----------------------------------------------------------
+rm(diet_missing,diet_prop,ems_diet_all,ems_diet_prop,ems_diet_zero,ems_diet_effort,
+   ems_prey,ems_prey_all,ems_prey_select,ems_prey_zero,ems_prey_effort,envir_prop_all,
+   envir_sept_prop,envir_may_prop,envir_missing,fid_list,prey_list,region_list,
+   serial_list)
+
+## -----------------------------------------------------------
+## Rearrange data frame for paired t-test
+## -----------------------------------------------------------
+## Add zero values for consitent pairs among months and basins
+## Apply first loop function
+output1 <- data.frame(do.call(rbind,lapply(month_list,function(i) {
+  ## Filter catch by each serial
+  select_filt <- alpha %>% filter(month==i)
+  ## Apply second loop function
+  tmp <- lapply(basin_list,function(j) {
+    ## Filter by each species and drop all other factor levels
+    select_filt2 <- select_filt %>% filter(basin==j) %>% 
+      droplevels()
+    ## True/false output if life stages does not exist (zero value life stages)
+    taxa <- diet_list[!diet_list %in% select_filt2$taxa]
+    ## Determine the number of life stages to be added
+    n <- length(taxa)
+    ## Create data frame with all zero value life stages, repeat by "n"
+    tmp <- data.frame(month=rep(i,n),basin=rep(j,n),taxa=taxa,alpha=rep(0,n))
+  })
+  ## Bind list into data frame
+  tmp2 <- data.frame(do.call(rbind,tmp))
+  
+  ## Bind all serials
+  select_all <- if((exists("select_all"))==F) {
+    tmp2 } else {
+      rbind(select_all,tmp2)
+    }
+  select_all
+})))
+## End double loop
+
+## Bind all zero values with non-zero values
+final_alpha <- bind_rows(alpha,output1) %>% 
+  arrange(month,basin,taxa) %>% 
+  mutate(basin=factor(basin,levels = c('Western','Central','Eastern'),ordered = TRUE))
+
+## -----------------------------------------------------------
+## paired t-test
+## -----------------------------------------------------------
+t.test(filter(final_alpha,basin=="Western",month=="May")$alpha,filter(final_alpha,basin=="Western",month=="September")$alpha,paired=T)
+t.test(filter(final_alpha,basin=="Central",month=="May")$alpha,filter(final_alpha,basin=="Central",month=="September")$alpha,paired=T)
+t.test(filter(final_alpha,basin=="Eastern",month=="May")$alpha,filter(final_alpha,basin=="Eastern",month=="September")$alpha,paired=T)
+
+## -----------------------------------------------------------
+## ANOVA
+## -----------------------------------------------------------
+alpha_taxa_lm <- lm(alpha~taxa,data=final_alpha)
+anova(alpha_taxa_lm)
+alpha_month_lm <- lm(alpha~taxa*month,data=final_alpha)
+anova(alpha_month_lm)
+alpha_basin_lm <- lm(alpha~taxa*basin,data=final_alpha)
+anova(alpha_basin_lm)
+
+final_alpha$grps <- final_alpha$month:final_alpha$taxa  # create single-factor interaction variable
+alpha_lm2 <- lm(alpha~grps,data=final_alpha) # fit new one-way ANOVA model
+alpha_mc2 <- glht(alpha_lm2,mcp(grps="Tukey"))
+summary(alpha_mc2)

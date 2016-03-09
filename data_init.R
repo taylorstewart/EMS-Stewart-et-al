dad616d0f45b78c11c33a709f1176d4b061a832d
taylorstewart <trstewart@usgs.gov>
2016-03-09 15:38:57 -0500

    _ to .

--- data_init.R
+++ data_init.R
@@ -24,6 +24,9 @@ library(FSA)       # ANCOVA functions
 library(car)       # assumption tests
 library(tidyr)     # tidy data
 library(nlstools)  # non-linear modeling
+library(grid)      # plot matrix text
+library(gridExtra) # plot matrix layout
+library(gtable)    # plot matrix legend
 
 ## ===========================================================
 ## Load and Initial Manipulations of the Fish Sample Data
@@ -31,40 +34,47 @@ library(nlstools)  # non-linear modeling
 ## -----------------------------------------------------------
 ## Load diet, calorimetry, and benthos data
 ## -----------------------------------------------------------
-ems_diet <- read_excel(path="data/CSMI_2014_EmeraldShiner.xlsx",sheet="Diet Summary") %>% 
+ems.diet <- read_excel(path="data/CSMI_2014_EmeraldShiner.xlsx",sheet="Diet Summary") %>% 
   mutate(fid=factor(fid),
          serial=factor(serial),
          basin=factor(basin,levels = c('Western','Central','Eastern'),ordered = TRUE),
-         biomass=as.numeric(mean_biomass_mg)) %>% 
-  select(-comments,-c(9:21))
-ems_cal <- read_excel(path="data/CSMI_2014_EmeraldShiner.xlsx",sheet="Calorimetry") %>% 
+         mean.size=as.numeric(mean.length.mm),
+         biomass=as.numeric(mean.biomass.mg)) %>%
+  select(-comments,-c(9:21)) %>% 
+  filter(food.item != "Cercopagididae",food.item != "Sididae",food.item != "Hydrachnidiae",food.item != "Harpacticoida")
+ems.cal <- read_excel(path="data/CSMI_2014_EmeraldShiner.xlsx",sheet="Calorimetry") %>% 
   mutate(fid=factor(fid),
          serial=factor(serial),
+         month=factor(month),
          basin=factor(basin,levels = c('Western','Central','Eastern'),ordered = TRUE),
-         log_tl=log(tl),
-         log_wt=log(wet_wt),
-         log_hoc=log(wet_HOC_JG)) %>%
+         log.tl=log(tl),
+         log.wt=log(wet.wt),
+         log.hoc=log(wet.HOC.JG)) %>%
   filter(include=="Yes") %>% 
   select(-c(8:15),-timestamp,-include,-comments)
-ems_benthos <- read_excel(path="data/benthos/CSMI_2014_Benthos_Biomass.xlsx",sheet="Biomass") %>% 
-  select(serial=sample_id,month,region,basin,ems_taxa=item,biomass=mean_biomass_mg) %>%
-  filter(ems_taxa!="Empty Sample") %>% 
-  group_by(serial,month,region,basin,ems_taxa) %>% 
-  summarize(biomass=sum(biomass)) %>% ungroup() %>% 
+ems.benthos <- read_excel(path="data/benthos/CSMI_2014_Benthos_Biomass.xlsx",sheet="Biomass") %>% 
+  select(serial=sample.id,month,region,basin,ems.taxa=item,mean.size=mean.length.mm,biomass=mean.biomass.mg) %>%
+  filter(ems.taxa!="Empty Sample") %>% 
+  group_by(serial,month,region,basin,ems.taxa) %>% 
+  summarize(mean.size=mean(mean.size),
+            biomass=sum(biomass)) %>% ungroup() %>% 
   mutate(serial=factor(serial))
+ems.sia <- read_excel(path="data/stable_isotope/CSMI_2014_SIA_EMS.xlsx",sheet="C&N") %>% 
+  select(-region)
 
 ## -----------------------------------------------------------
 ## rename some diet species and summarise multiple taxa within the same fish
 ## -----------------------------------------------------------
-ems_diet$food_item <- gsub("nauplii","Cyclopoida",ems_diet$food_item)
-ems_diet %<>% group_by(fid,serial,month,region,basin,tl,w_wt,food_item) %>% 
-  summarise(biomass=sum(biomass)) %>% ungroup()
+ems.diet$food.item <- gsub("nauplii","Cyclopoida",ems.diet$food.item)
+ems.diet %<>% group_by(fid,serial,month,region,basin,tl,w.wt,food.item) %>% 
+  summarise(mean.size=mean(mean.size),
+            biomass=sum(biomass)) %>% ungroup()
 
 ## -----------------------------------------------------------
 ## rename some benthos species
 ## -----------------------------------------------------------
-ems_benthos$ems_taxa <- gsub("Quagga mussel","Dreissenidae",ems_benthos$ems_taxa)
-ems_benthos$ems_taxa <- gsub("Zebra mussel","Dreissenidae",ems_benthos$ems_taxa)
+ems.benthos$ems.taxa <- gsub("Quagga mussel","Dreissenidae",ems.benthos$ems.taxa)
+ems.benthos$ems.taxa <- gsub("Zebra mussel","Dreissenidae",ems.benthos$ems.taxa)
 
 ## ===========================================================
 ## Zooplankton Data Manipulation
@@ -72,35 +82,35 @@ ems_benthos$ems_taxa <- gsub("Zebra mussel","Dreissenidae",ems_benthos$ems_taxa)
 ## -----------------------------------------------------------
 ## Load rep1 zooplankton data
 ## -----------------------------------------------------------
-zoop_pred_biom <- read_excel("data/zooplankton/2014_CSMI_Zp_Pred_Clad_Rep1.xlsx","Biom",skip=3) %>% 
+zoop.pred.biom <- read_excel("data/zooplankton/2014_CSMI_Zp_Pred_Clad_Rep1.xlsx","Biom",skip=3) %>% 
   slice(2:155) %>% 
   select(-c(63:77,132:145,149:167))  #drop columns of egg counts and other noise
-zoop_sub_biom <- read_excel("data/zooplankton/2014_CSMI_Zp_Sub_Rep1.xlsx","Biom",skip=3) %>%
+zoop.sub.biom <- read_excel("data/zooplankton/2014_CSMI_Zp_Sub_Rep1.xlsx","Biom",skip=3) %>%
   slice(-1) %>% 
   select(-c(63:77,132:145,149:167))  #drop columns of egg counts and other noise
-zoop_pred_dens <- read_excel("data/zooplankton/2014_CSMI_Zp_Pred_Clad_Rep1.xlsx","Dens",skip=3) %>% 
+zoop.pred.dens <- read_excel("data/zooplankton/2014_CSMI_Zp_Pred_Clad_Rep1.xlsx","Dens",skip=3) %>% 
   slice(2:155) %>% 
   select(-c(63:77,132:145,149:167))  #drop columns of egg counts and other noise
-zoop_sub_dens <- read_excel("data/zooplankton/2014_CSMI_Zp_Sub_Rep1.xlsx","Dens",skip=3) %>%
+zoop.sub.dens <- read_excel("data/zooplankton/2014_CSMI_Zp_Sub_Rep1.xlsx","Dens",skip=3) %>%
   slice(-1) %>% 
   select(-c(63:77,132:145,149:167))  #drop columns of egg counts and other noise
 
 ## -----------------------------------------------------------
 ## reshape data into key(species)-value pairs
 ## -----------------------------------------------------------
-tidy_zoop_pred_biom <- zoop_pred_biom %>%
+tidy.zoop.pred.biom <- zoop.pred.biom %>%
   ## remove samples with issues (i.e. flowmeter, net under boat, etc.)
   filter(!(SampleLocation %in% c("5096Fair","5103MB","5124Fair","5131Erie","5130Erie","5240Lor","5248Fair"))) %>% 
   gather(species,biomass,-SampleDate,-SampleLocation)
-tidy_zoop_sub_biom <- zoop_sub_biom %>%
+tidy.zoop.sub.biom <- zoop.sub.biom %>%
   ## remove samples with issues (i.e. flowmeter, net under boat, etc.)
   filter(!(SampleLocation %in% c("5096Fair","5103MB","5124Fair","5131Erie","5130Erie","5240Lor","5248Fair"))) %>% 
   gather(species,biomass,-SampleDate,-SampleLocation)
-tidy_zoop_pred_dens <- zoop_pred_dens %>%
+tidy.zoop.pred.dens <- zoop.pred.dens %>%
   ## remove samples with issues (i.e. flowmeter, net under boat, etc.)
   filter(!(SampleLocation %in% c("5096Fair","5103MB","5124Fair","5131Erie","5130Erie","5240Lor","5248Fair"))) %>% 
   gather(species,density,-SampleDate,-SampleLocation)
-tidy_zoop_sub_dens <- zoop_sub_dens %>%
+tidy.zoop.sub.dens <- zoop.sub.dens %>%
   ## remove samples with issues (i.e. flowmeter, net under boat, etc.)
   filter(!(SampleLocation %in% c("5096Fair","5103MB","5124Fair","5131Erie","5130Erie","5240Lor","5248Fair"))) %>% 
   gather(species,density,-SampleDate,-SampleLocation)
@@ -108,40 +118,40 @@ tidy_zoop_sub_dens <- zoop_sub_dens %>%
 ## -----------------------------------------------------------
 ## remove "w/ eggs" and "w/o eggs" from column names
 ## -----------------------------------------------------------
-tidy_zoop_pred_biom$species <- gsub(" w/ eggs","",tidy_zoop_pred_biom$species)
-tidy_zoop_pred_biom$species <- gsub("w/ eggs","",tidy_zoop_pred_biom$species)
-tidy_zoop_pred_biom$species <- gsub(" w/o eggs","",tidy_zoop_pred_biom$species)
-tidy_zoop_pred_biom$species <- gsub("w/o eggs","",tidy_zoop_pred_biom$species)
-tidy_zoop_sub_biom$species <- gsub(" w/ eggs","",tidy_zoop_sub_biom$species)
-tidy_zoop_sub_biom$species <- gsub("w/ eggs","",tidy_zoop_sub_biom$species)
-tidy_zoop_sub_biom$species <- gsub(" w/o eggs","",tidy_zoop_sub_biom$species)
-tidy_zoop_sub_biom$species <- gsub("w/o eggs","",tidy_zoop_sub_biom$species)
-tidy_zoop_pred_dens$species <- gsub(" w/ eggs","",tidy_zoop_pred_dens$species)
-tidy_zoop_pred_dens$species <- gsub("w/ eggs","",tidy_zoop_pred_dens$species)
-tidy_zoop_pred_dens$species <- gsub(" w/o eggs","",tidy_zoop_pred_dens$species)
-tidy_zoop_pred_dens$species <- gsub("w/o eggs","",tidy_zoop_pred_dens$species)
-tidy_zoop_sub_dens$species <- gsub(" w/ eggs","",tidy_zoop_sub_dens$species)
-tidy_zoop_sub_dens$species <- gsub("w/ eggs","",tidy_zoop_sub_dens$species)
-tidy_zoop_sub_dens$species <- gsub(" w/o eggs","",tidy_zoop_sub_dens$species)
-tidy_zoop_sub_dens$species <- gsub("w/o eggs","",tidy_zoop_sub_dens$species)
+tidy.zoop.pred.biom$species <- gsub(" w/ eggs","",tidy.zoop.pred.biom$species)
+tidy.zoop.pred.biom$species <- gsub("w/ eggs","",tidy.zoop.pred.biom$species)
+tidy.zoop.pred.biom$species <- gsub(" w/o eggs","",tidy.zoop.pred.biom$species)
+tidy.zoop.pred.biom$species <- gsub("w/o eggs","",tidy.zoop.pred.biom$species)
+tidy.zoop.sub.biom$species <- gsub(" w/ eggs","",tidy.zoop.sub.biom$species)
+tidy.zoop.sub.biom$species <- gsub("w/ eggs","",tidy.zoop.sub.biom$species)
+tidy.zoop.sub.biom$species <- gsub(" w/o eggs","",tidy.zoop.sub.biom$species)
+tidy.zoop.sub.biom$species <- gsub("w/o eggs","",tidy.zoop.sub.biom$species)
+tidy.zoop.pred.dens$species <- gsub(" w/ eggs","",tidy.zoop.pred.dens$species)
+tidy.zoop.pred.dens$species <- gsub("w/ eggs","",tidy.zoop.pred.dens$species)
+tidy.zoop.pred.dens$species <- gsub(" w/o eggs","",tidy.zoop.pred.dens$species)
+tidy.zoop.pred.dens$species <- gsub("w/o eggs","",tidy.zoop.pred.dens$species)
+tidy.zoop.sub.dens$species <- gsub(" w/ eggs","",tidy.zoop.sub.dens$species)
+tidy.zoop.sub.dens$species <- gsub("w/ eggs","",tidy.zoop.sub.dens$species)
+tidy.zoop.sub.dens$species <- gsub(" w/o eggs","",tidy.zoop.sub.dens$species)
+tidy.zoop.sub.dens$species <- gsub("w/o eggs","",tidy.zoop.sub.dens$species)
 
 ## -----------------------------------------------------------
 ## combine values from subsample data and whole count predatory data
 ## -----------------------------------------------------------
-zoop_pred <- left_join(tidy_zoop_pred_biom,tidy_zoop_pred_dens,by=c("SampleDate", "SampleLocation", "species"))
-zoop_sub <- left_join(tidy_zoop_sub_biom,tidy_zoop_sub_dens,by=c("SampleDate", "SampleLocation", "species"))
-all_zoop <- bind_rows(zoop_pred,zoop_sub) %>%
+zoop.pred <- left_join(tidy.zoop.pred.biom,tidy.zoop.pred.dens,by=c("SampleDate", "SampleLocation", "species"))
+zoop.sub <- left_join(tidy.zoop.sub.biom,tidy.zoop.sub.dens,by=c("SampleDate", "SampleLocation", "species"))
+all.zoop <- bind_rows(zoop.pred,zoop.sub) %>%
   mutate(SampleDate=factor(SampleDate),
          SampleLocation=factor(SampleLocation),
-         value_den=as.numeric(density),
-         value_bio=as.numeric(biomass))
+         value.den=as.numeric(density),
+         value.bio=as.numeric(biomass))
 
 ## -----------------------------------------------------------
 ## summarize values and filter to serials where diets were sampled from
 ## -----------------------------------------------------------
-all_zoop %<>% group_by(SampleDate,SampleLocation,species) %>% 
-  summarise(density=sum(value_den),
-            biomass=sum(value_bio)) %>% 
+all.zoop %<>% group_by(SampleDate,SampleLocation,species) %>% 
+  summarise(density=sum(value.den),
+            biomass=sum(value.bio)) %>% 
   filter(biomass > 0, density > 0) %>% ungroup() %>% 
   mutate(serial=as.numeric(substr(SampleLocation,1,4))) %>% 
   select(serial,species,density,biomass) %>% 
@@ -151,24 +161,24 @@ all_zoop %<>% group_by(SampleDate,SampleLocation,species) %>%
 ## Now read in effort data so can merge in region, depth, lat, long, etc.
 ## -----------------------------------------------------------
 ef <- read_excel("data/zooplankton/CSMI_Zp_Effort.xlsx",sheet="Effort") %>% 
-  select(-depth_strata,-depth_ft,-depth_m,-date,-longitude,-latitude)
+  select(-depth.strata,-depth.ft,-depth.m,-date,-longitude,-latitude)
 
 ## -----------------------------------------------------------
 ## merge zoop data and effort data
 ## -----------------------------------------------------------
-zoop_effort <- inner_join(ef,all_zoop,by="serial")
+zoop.effort <- inner_join(ef,all.zoop,by="serial")
 
 ## -----------------------------------------------------------
 ## read in taxamonic data and join with zoop data
 ## -----------------------------------------------------------
 taxa <- read_excel("data/zooplankton/CSMI_Zp_Taxonomy.xlsx",sheet="Taxa")
-ems_zoop <- left_join(zoop_effort,taxa,by="species") %>% 
-  select(serial,month,region,basin,ems_taxa,biomass) %>% 
-  group_by(serial,month,region,basin,ems_taxa) %>% 
+ems.zoop <- left_join(zoop.effort,taxa,by="species") %>% 
+  select(serial,month,region,basin,ems.taxa,biomass) %>% 
+  group_by(serial,month,region,basin,ems.taxa) %>% 
   summarize(biomass=sum(biomass))
 
 ## ===========================================================
 ## clean up environment
 ## ===========================================================
-rm(all_zoop,ef,taxa,tidy_zoop_pred_biom,tidy_zoop_sub_biom,tidy_zoop_pred_dens,tidy_zoop_sub_dens,
-   zoop_effort,zoop_pred_biom,zoop_sub_biom,zoop_pred_dens,zoop_sub_dens,zoop_sub,zoop_pred)
+rm(all.zoop,ef,taxa,tidy.zoop.pred.biom,tidy.zoop.sub.biom,tidy.zoop.pred.dens,tidy.zoop.sub.dens,
+   zoop.effort,zoop.pred.biom,zoop.sub.biom,zoop.pred.dens,zoop.sub.dens,zoop.sub,zoop.pred)

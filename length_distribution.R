e5c95d951309c56cbff675f8f6e5fd46f7335b49
taylorstewart <trstewart@usgs.gov>
2016-03-09 15:39:36 -0500

    additional sample size calculations

--- length_distribution.R
+++ length_distribution.R
@@ -1,13 +1,29 @@
 source("data_init.R")
 
+## cal tl
 ggplot(ems_cal,aes(tl)) +
   geom_histogram(binwidth=10) +
   scale_x_continuous(limits=c(40,100)) +
   theme_bw() +
   facet_wrap(month~basin)
 
+## cal wt
+ggplot(ems_cal,aes(wet_wt)) +
+  geom_histogram(binwidth=1) +
+  scale_x_continuous(limits=c(0,7)) +
+  theme_bw() +
+  facet_wrap(month~basin)
+
+## diet tl
 ggplot(filter(ems_diet,food_item!="Empty"),aes(tl)) +
   geom_histogram(binwidth=10) +
   scale_x_continuous(limits=c(40,100)) +
+  theme_bw() +
+  facet_wrap(month~basin)
+
+## diet wt
+ggplot(filter(ems_diet,food_item!="Empty"),aes(w_wt)) +
+  geom_histogram(binwidth=1) +
+  scale_x_continuous(limits=c(0,7)) +
   theme_bw() +
   facet_wrap(month~basin)

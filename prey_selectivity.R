6679aab2702df79f1e4bf3cbe4bea138a75aa066
taylorstewart <trstewart@usgs.gov>
2016-03-09 15:40:03 -0500

    added plot.

--- prey_selectivity.R
+++ prey_selectivity.R
@@ -10,173 +10,174 @@
 ## Source Data_Init Script ... get ems_cal data.frame
 ## ===========================================================
 source("data_init.R")
-str(ems_diet)
-str(ems_zoop)
-str(ems_benthos)
+str(ems.diet)
+str(ems.zoop)
+str(ems.benthos)
 
 ## -----------------------------------------------------------
 ## Remove stomachs with no diet contents
 ## -----------------------------------------------------------
-ems_diet %<>% filter(food_item != "Empty")
+ems.diet %<>% filter(food.item != "Empty")
 
 ## -----------------------------------------------------------
 ## Combine benthos and zooplankton prey
 ## -----------------------------------------------------------
-ems_prey <- bind_rows(ems_zoop,ems_benthos) %>% 
-  group_by(month,region,basin,ems_taxa) %>% 
+ems.prey <- bind_rows(ems.zoop,ems.benthos) %>% 
+  group_by(month,region,basin,ems.taxa) %>% 
   summarize(biomass=sum(biomass))
 
 ## -----------------------------------------------------------
 ## Create unique lists of serials sampled, FID, diet prey, and available prey
 ## -----------------------------------------------------------
-serial_list <- as.character(unique(ems_diet$serial))
-region_list <- as.character(unique(ems_prey$region))
-basin_list <- as.character(unique(ems_prey$basin))
-month_list <-  c('May','September')
-fid_list <- unique(ems_diet$fid)
-diet_list <- unique(ems_diet$food_item)
-prey_list <- unique(ems_prey$ems_taxa)
+serial.list <- as.character(unique(ems.diet$serial))
+region.list <- as.character(unique(ems.prey$region))
+basin.list <- as.character(unique(ems.prey$basin))
+month.list <-  c('May','September')
+fid.list <- unique(ems.diet$fid)
+diet.list <- unique(ems.diet$food.item)
+prey.list <- unique(ems.prey$ems.taxa)
 
 ## -----------------------------------------------------------
 ## Filter prey taxa to only indentified taxa
 ## -----------------------------------------------------------
-ems_prey %<>% filter(ems_taxa %in% diet_list, month %in% month_list)
+ems.prey %<>% filter(ems.taxa %in% diet.list, month %in% month.list)
 
 ## -----------------------------------------------------------
 ## Create a loop function to add zeros for all prey taxa missing from each region
 ## -----------------------------------------------------------
 ## Apply loop function
-diet_missing <- data.frame(do.call(rbind,lapply(fid_list,function(i) {
+diet.missing <- data.frame(do.call(rbind,lapply(fid.list,function(i) {
   ## Filter catch by each serial
-  fid2 <- ems_diet %>% filter(fid==i)
+  fid2 <- ems.diet %>% filter(fid==i)
   serial <- unique(as.character(fid2$serial))
   region <- unique(as.character(fid2$region))
   ## True/false output if life stages does not exist (zero value life stages)
-  pl <- diet_list[!diet_list %in% fid2$food_item]
+  pl <- diet.list[!diet.list %in% fid2$food.item]
   ## Determine the number of life stages to be added
   n <- length(pl)
   ## Create data frame with all zero value life stages, repeat by "n"
-  tmp <- data.frame(fid=rep(i,n),serial=serial,region=region,food_item=pl,biomass=rep(0,n))
+  tmp <- data.frame(fid=rep(i,n),serial=serial,region=region,food.item=pl,biomass=rep(0,n))
 })))
 
 ## -----------------------------------------------------------
 ## Join zero data by serial to fill in other variables
 ## -----------------------------------------------------------
-ems_diet_effort <- ems_diet %>% distinct(serial,month,region,basin) %>% select(serial,month,region,basin)
-ems_diet_zero <- left_join(diet_missing,ems_diet_effort)
-ems_diet_all <- bind_rows(ems_diet,ems_diet_zero) %>% arrange(fid)
+ems.diet.effort <- ems.diet %>% distinct(serial,month,region,basin) %>% select(serial,month,region,basin)
+ems.diet.zero <- left_join(diet.missing,ems.diet.effort)
+ems.diet.all <- bind_rows(ems.diet,ems.diet.zero) %>% arrange(fid)
 
 ## -----------------------------------------------------------
 ## Create a loop function to add zeros for all prey taxa missing from each serial
 ## -----------------------------------------------------------
 ## Apply loop function
-envir_missing <-  data.frame(do.call(rbind,lapply(month_list,function(g){
-  month <- ems_prey %>% filter(month == g)
-  tmp <- data.frame(do.call(rbind,lapply(region_list,function(i) {
+envir.missing <-  data.frame(do.call(rbind,lapply(month.list,function(g){
+  month <- ems.prey %>% filter(month == g)
+  tmp <- data.frame(do.call(rbind,lapply(region.list,function(i) {
     ## Filter catch by each region
     region2 <- month %>% filter(region==i)
       ## True/false output if life stages does not exist (zero value life stages)
-      pl <- diet_list[!diet_list %in% region2$ems_taxa]
+      pl <- diet.list[!diet.list %in% region2$ems.taxa]
       ## Determine the number of life stages to be added
       n <- length(pl)
       ## Create data frame with all zero value life stages, repeat by "n"
-      data.frame(month=rep(g,n),region=rep(i,n),ems_taxa=pl,biomass=rep(0,n))
+      data.frame(month=rep(g,n),region=rep(i,n),ems.taxa=pl,biomass=rep(0,n))
     })))
 })))
 
 ## -----------------------------------------------------------
 ## Join zero data by serial to fill in other variables
 ## -----------------------------------------------------------
-ems_prey_effort <- ems_prey %>% distinct(month,region,basin) %>% select(month,region,basin)
-ems_prey_zero <- left_join(envir_missing,ems_prey_effort)
-ems_prey_all <- bind_rows(ems_prey,ems_prey_zero) %>% arrange(month,region)
+ems.prey.effort <- ems.prey %>% distinct(month,region,basin) %>% select(month,region,basin)
+ems.prey.zero <- left_join(envir.missing,ems.prey.effort)
+ems.prey.all <- bind_rows(ems.prey,ems.prey.zero) %>% arrange(month,region)
 
 ## -----------------------------------------------------------
 ## Calculate the proportion of prey available in the environment
 ## -----------------------------------------------------------
-diet_prop <- as.data.frame(do.call(rbind,lapply(fid_list,function(i) {
-  indiv <- ems_diet_all %>% filter(fid == i)
-  prop <- as.data.frame(do.call(rbind,lapply(diet_list,function(j) {
-    round((filter(indiv,food_item == j)$biomass)/sum(indiv$biomass),4)
+diet.prop <- as.data.frame(do.call(rbind,lapply(fid.list,function(i) {
+  indiv <- ems.diet.all %>% filter(fid == i)
+  prop <- as.data.frame(do.call(rbind,lapply(diet.list,function(j) {
+    round((filter(indiv,food.item == j)$biomass)/sum(indiv$biomass),4)
   })))
-  data.frame(fid=rep(i,13),food_item=diet_list,diet_prop=prop)
+  data.frame(fid=rep(i,9),food.item=diet.list,diet.prop=prop)
 }))) %>% 
-  mutate(diet_prop=V1) %>% select(-V1)
+  mutate(diet.prop=V1) %>% select(-V1)
 
-ems_diet_prop <- left_join(ems_diet,diet_prop)
+ems.diet.prop <- left_join(ems.diet,diet.prop)
 
 ## -----------------------------------------------------------
 ## Calculate the proportion of prey available in the environment
 ## -----------------------------------------------------------
-envir_may_prop <- as.data.frame(do.call(rbind,lapply(region_list,function(i) {
-  indiv <- ems_prey_all %>% filter(region == i,month == "May")
-  prop <- as.data.frame(do.call(rbind,lapply(diet_list,function(j) {
-    round((filter(indiv,ems_taxa == j)$biomass)/sum(indiv$biomass),4)
+envir.may.prop <- as.data.frame(do.call(rbind,lapply(region.list,function(i) {
+  indiv <- ems.prey.all %>% filter(region == i,month == "May")
+  prop <- as.data.frame(do.call(rbind,lapply(diet.list,function(j) {
+    round((filter(indiv,ems.taxa == j)$biomass)/sum(indiv$biomass),4)
   })))
-  data.frame(month=rep("May",13),region=rep(i,13),food_item=diet_list,envir_prop=prop)
+  data.frame(month=rep("May",9),region=rep(i,9),food.item=diet.list,envir.prop=prop)
 }))) %>% 
-  mutate(envir_prop=V1) %>% select(-V1)
+  mutate(envir.prop=V1) %>% select(-V1)
 
-envir_sept_prop <- as.data.frame(do.call(rbind,lapply(region_list,function(i) {
-  indiv <- ems_prey_all %>% filter(region == i,month == "September")
-  prop <- as.data.frame(do.call(rbind,lapply(diet_list,function(j) {
-    round((filter(indiv,ems_taxa == j)$biomass)/sum(indiv$biomass),4)
+envir.sept.prop <- as.data.frame(do.call(rbind,lapply(region.list,function(i) {
+  indiv <- ems.prey.all %>% filter(region == i,month == "September")
+  prop <- as.data.frame(do.call(rbind,lapply(diet.list,function(j) {
+    round((filter(indiv,ems.taxa == j)$biomass)/sum(indiv$biomass),4)
   })))
-  data.frame(month=rep("September",13),region=rep(i,13),food_item=diet_list,envir_prop=prop)
+  data.frame(month=rep("September",9),region=rep(i,9),food.item=diet.list,envir.prop=prop)
 }))) %>% 
-  mutate(envir_prop=V1) %>% select(-V1)
+  mutate(envir.prop=V1) %>% select(-V1)
 
-envir_prop_all <- bind_rows(envir_may_prop,envir_sept_prop)
-ems_prey_select <- left_join(ems_diet_prop,envir_prop_all)
+envir.prop.all <- bind_rows(envir.may.prop,envir.sept.prop)
+ems.prey.select <- left_join(ems.diet.prop,envir.prop.all)
 
 ## -----------------------------------------------------------
 ## Divide proportion of prey available in the environment by proportion of prey in diet
 ## -----------------------------------------------------------
-ems_prey_select %<>% mutate("diet/envir" = diet_prop/envir_prop)
+ems.prey.select %<>% mutate("diet/envir" = diet.prop/envir.prop)
 
 ## -----------------------------------------------------------
 ## Calculate alpha ((r/p)/sum(r/p))
 ## -----------------------------------------------------------
-alpha <- data.frame(do.call(rbind,lapply(fid_list,function(k) {
-  indiv <- ems_prey_select %>% filter(fid == k)
+alpha <- data.frame(do.call(rbind,lapply(fid.list,function(k) {
+  indiv <- ems.prey.select %>% filter(fid == k)
   serial <- unique(as.character(indiv$serial))
   month <- unique(as.character(indiv$month))
   basin <- unique(as.character(indiv$basin))
-  taxa <- as.character(indiv$food_item)
+  taxa <- as.character(indiv$food.item)
   n <- nrow(indiv)
     tmp <- data.frame(do.call(rbind,lapply(1:n,function(p) {
       round(indiv[p,12]/sum(indiv[,12]),5)
     })))
-  data.frame(fid=rep(k,n),serial,month,basin,taxa,alpha=tmp$diet.envir)
+  data.frame(fid=rep(k,n),serial,month,basin,taxa,alpha=tmp$envir.prop)
 })))
 ## Remove NAs and summarize mean alpha by month, basin, and taxa
 alpha %<>% filter(!is.na(alpha)) %>% 
   group_by(month,basin,taxa) %>% 
-  summarize(alpha=mean(alpha))
+  summarize(alpha=mean(alpha)) %>% 
+  arrange(desc(taxa))
 
 ## -----------------------------------------------------------
 ## Clean up environment
 ## -----------------------------------------------------------
-rm(diet_missing,diet_prop,ems_diet_all,ems_diet_prop,ems_diet_zero,ems_diet_effort,
-   ems_prey,ems_prey_all,ems_prey_select,ems_prey_zero,ems_prey_effort,envir_prop_all,
-   envir_sept_prop,envir_may_prop,envir_missing,fid_list,prey_list,region_list,
-   serial_list)
+rm(diet.missing,diet.prop,ems.diet.all,ems.diet.prop,ems.diet.zero,ems.diet.effort,
+   ems.prey,ems.prey.all,ems.prey.select,ems.prey.zero,ems.prey.effort,envir.prop.all,
+   envir.sept.prop,envir.may.prop,envir.missing,fid.list,prey.list,region.list,
+   serial.list)
 
 ## -----------------------------------------------------------
 ## Rearrange data frame for paired t-test
 ## -----------------------------------------------------------
 ## Add zero values for consitent pairs among months and basins
 ## Apply first loop function
-output1 <- data.frame(do.call(rbind,lapply(month_list,function(i) {
+output1 <- data.frame(do.call(rbind,lapply(month.list,function(i) {
   ## Filter catch by each serial
-  select_filt <- alpha %>% filter(month==i)
+  select.filt <- alpha %>% filter(month==i)
   ## Apply second loop function
-  tmp <- lapply(basin_list,function(j) {
+  tmp <- lapply(basin.list,function(j) {
     ## Filter by each species and drop all other factor levels
-    select_filt2 <- select_filt %>% filter(basin==j) %>% 
+    select.filt2 <- select.filt %>% filter(basin==j) %>% 
       droplevels()
     ## True/false output if life stages does not exist (zero value life stages)
-    taxa <- diet_list[!diet_list %in% select_filt2$taxa]
+    taxa <- diet.list[!diet.list %in% select.filt2$taxa]
     ## Determine the number of life stages to be added
     n <- length(taxa)
     ## Create data frame with all zero value life stages, repeat by "n"
@@ -186,37 +187,112 @@ output1 <- data.frame(do.call(rbind,lapply(month_list,function(i) {
   tmp2 <- data.frame(do.call(rbind,tmp))
   
   ## Bind all serials
-  select_all <- if((exists("select_all"))==F) {
+  select.all <- if((exists("select.all"))==F) {
     tmp2 } else {
-      rbind(select_all,tmp2)
+      rbind(select.all,tmp2)
     }
-  select_all
+  select.all
 })))
 ## End double loop
 
 ## Bind all zero values with non-zero values
-final_alpha <- bind_rows(alpha,output1) %>% 
+final.alpha <- bind_rows(alpha,output1) %>% 
   arrange(month,basin,taxa) %>% 
   mutate(basin=factor(basin,levels = c('Western','Central','Eastern'),ordered = TRUE))
+## remove extra objects
+rm(alpha,output1,basin.list,diet.list,month.list)
 
 ## -----------------------------------------------------------
-## paired t-test
+## abbreviate taxon
 ## -----------------------------------------------------------
-t.test(filter(final_alpha,basin=="Western",month=="May")$alpha,filter(final_alpha,basin=="Western",month=="September")$alpha,paired=T)
-t.test(filter(final_alpha,basin=="Central",month=="May")$alpha,filter(final_alpha,basin=="Central",month=="September")$alpha,paired=T)
-t.test(filter(final_alpha,basin=="Eastern",month=="May")$alpha,filter(final_alpha,basin=="Eastern",month=="September")$alpha,paired=T)
+final.alpha$taxa <- gsub('Calanoida','CA',final.alpha$taxa)
+final.alpha$taxa <- gsub('Cyclopoida','CY',final.alpha$taxa)
+final.alpha$taxa <- gsub('Chironomid Larvae','CL',final.alpha$taxa)
+final.alpha$taxa <- gsub('Bosminidae','BO',final.alpha$taxa)
+final.alpha$taxa <- gsub('Leptodoridae','LE',final.alpha$taxa)
+final.alpha$taxa <- gsub('Nematoda','NE',final.alpha$taxa)
+final.alpha$taxa <- gsub('Chironomid Pupae','CP',final.alpha$taxa)
+final.alpha$taxa <- gsub('Daphnidae','DA',final.alpha$taxa)
+final.alpha$taxa <- gsub('Oligochaeta','OL',final.alpha$taxa)
+
+final.alpha %<>% arrange(month,basin,taxa)
 
 ## -----------------------------------------------------------
-## ANOVA
+## season paired t-test
 ## -----------------------------------------------------------
-alpha_taxa_lm <- lm(alpha~taxa,data=final_alpha)
-anova(alpha_taxa_lm)
-alpha_month_lm <- lm(alpha~taxa*month,data=final_alpha)
-anova(alpha_month_lm)
-alpha_basin_lm <- lm(alpha~taxa*basin,data=final_alpha)
-anova(alpha_basin_lm)
+t.test(filter(final.alpha,basin=="Western",month=="May")$alpha,filter(final.alpha,basin=="Western",month=="September")$alpha,paired=T)
+t.test(filter(final.alpha,basin=="Central",month=="May")$alpha,filter(final.alpha,basin=="Central",month=="September")$alpha,paired=T)
+t.test(filter(final.alpha,basin=="Eastern",month=="May")$alpha,filter(final.alpha,basin=="Eastern",month=="September")$alpha,paired=T)
 
-final_alpha$grps <- final_alpha$month:final_alpha$taxa  # create single-factor interaction variable
-alpha_lm2 <- lm(alpha~grps,data=final_alpha) # fit new one-way ANOVA model
-alpha_mc2 <- glht(alpha_lm2,mcp(grps="Tukey"))
-summary(alpha_mc2)
+## -----------------------------------------------------------
+## basin post-hoc pairwise t-test
+## -----------------------------------------------------------
+pairwise.t.test(filter(final.alpha,month=="May")$alpha,filter(final.alpha,month=="May")$basin,paired=T,p.adjust.method="bonferroni")
+pairwise.t.test(filter(final.alpha,month=="September")$alpha,filter(final.alpha,month=="September")$basin,paired=T,p.adjust.method="bonferroni")
+pairwise.t.test(final.alpha$alpha,final.alpha$basin,paired=T,p.adjust.method="bonferroni")
+
+## -----------------------------------------------------------
+## visualization
+## -----------------------------------------------------------
+may.west <- ggplot(filter(final.alpha,month=="May",basin=="Western"),aes(taxa,alpha)) +
+  geom_bar(stat="identity") +
+  labs(x="",y="",title="May Western") +
+  scale_y_continuous(limit=c(0,1),expand=c(0,0)) +
+  theme(axis.line.y=element_line(),axis.line.x=element_line(),
+        axis.text.y=element_text(size=15),axis.text.x=element_blank(),panel.background=element_blank(),
+        plot.margin=unit(c(5,2,-4,0),"mm"))
+
+may.cen <- ggplot(filter(final.alpha,month=="May",basin=="Central"),aes(taxa,alpha)) +
+  geom_bar(stat="identity") +
+  labs(x="",y="",title="May Central") +
+  scale_y_continuous(limit=c(0,1),expand=c(0,0)) +
+  theme(axis.line.y=element_line(),axis.line.x=element_line(),
+        axis.text.y=element_blank(),axis.text.x=element_blank(),panel.background=element_blank(),
+        plot.margin=unit(c(5,2,-4,4),"mm"))
+
+may.east <- ggplot(filter(final.alpha,month=="May",basin=="Eastern"),aes(taxa,alpha)) +
+  geom_bar(stat="identity") +
+  labs(x="",y="",title="May Eastern") +
+  scale_y_continuous(limit=c(0,1),expand=c(0,0)) +
+  theme(axis.line.y=element_line(),axis.line.x=element_line(),
+        axis.text.y=element_blank(),axis.text.x=element_blank(),panel.background=element_blank(),
+        plot.margin=unit(c(5,2,-4,4),"mm"))
+
+sept.west <- ggplot(filter(final.alpha,month=="September",basin=="Western"),aes(taxa,alpha)) +
+  geom_bar(stat="identity") +
+  labs(x="",y="",title="Sept Western ") +
+  scale_y_continuous(limit=c(0,1),expand=c(0,0)) +
+  theme(axis.line.y=element_line(),axis.line.x=element_line(),
+        axis.text.x=element_text(size=13),
+        axis.text.y=element_text(size=15),panel.background=element_blank(),plot.margin=unit(c(11,2,-15,0),"mm"))
+
+sept.cen <- ggplot(filter(final.alpha,month=="September",basin=="Central"),aes(taxa,alpha)) +
+  geom_bar(stat="identity") +
+  labs(x="",y="",title="Sept Central") +
+  scale_y_continuous(limit=c(0,1),expand=c(0,0)) +
+  theme(axis.line.y=element_line(),axis.line.x=element_line(),
+        axis.text.x=element_text(size=13),
+        axis.text.y=element_blank(),panel.background=element_blank(),plot.margin=unit(c(11,2,-15,4),"mm"))
+
+sept.east <- ggplot(filter(final.alpha,month=="September",basin=="Eastern"),aes(taxa,alpha)) +
+  geom_bar(stat="identity") +
+  labs(x="",y="",title="Sept Eastern") +
+  scale_y_continuous(limit=c(0,1),expand=c(0,0)) +
+  theme(axis.line.y=element_line(),axis.line.x=element_line(),
+        axis.text.x=element_text(size=13),
+        axis.text.y=element_blank(),panel.background=element_blank(),plot.margin=unit(c(11,2,-15,4),"mm"))
+
+## -----------------------------------------------------------
+## Put plots into a matrix
+## -----------------------------------------------------------
+grid.arrange(arrangeGrob(may.west,
+                         may.cen,
+                         may.east,
+                         sept.west,
+                         sept.cen,
+                         sept.east,
+                         ncol=3,
+                         nrow=2,
+                         left=textGrob("Selectivity (W')",y=unit(90,'mm'),rot=90,gp=gpar(fontsize=20)),
+                         bottom=textGrob("Prey Type",y=unit(-15,'mm'),x=unit(135,'mm'),gp=gpar(fontsize=20))),
+             heights=c(8,1))
